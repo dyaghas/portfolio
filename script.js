a2981c2df6b112d231f2b9d85eab3624b4a09fb1
Gustavo Cristian <diamonddyaghas@gmail.com>
2023-04-26 16:06:57 -0300

    animations added

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ function hover(element) {
     element.setAttribute('src', 'images/github-mark.svg');
   }
   
-  function unhover(element) {
-    element.setAttribute('src', 'images/github-mark-white.svg');
-  }
+function unhover(element) {
+  element.setAttribute('src', 'images/github-mark-white.svg');
+}
+
+function copyToClipboard() {
+  navigator.clipboard.writeText("diamonddyaghas@gmail.com");
+}
