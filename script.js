7f3116759b1b7fac0f8ed0e7ac6648a3eae7949a
dyaghas <diamonddyaghas@gmail.com>
2024-01-25 12:48:51 -0300

    alterações visuais e de clique nos elementos da classe project-content e mudanças de conteúdo escrito

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-function hover(element) {
-    element.setAttribute('src', 'images/github-mark.svg');
-  }
-  
-function unhover(element) {
-  element.setAttribute('src', 'images/github-mark-white.svg');
-}
-
 function copyToClipboard() {
   navigator.clipboard.writeText("diamonddyaghas@gmail.com");
 }
